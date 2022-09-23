7a01be7a51e0237f3c12f86f66e8b19f9050c8ee
SyKeY XAM <46406915+SyKeYXAM@users.noreply.github.com>
2022-09-23 12:46:08 +0200

    Add files via upload

---
+++ Iniciar.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import os
+
+print ("1. Instalar programas necesarios")
+print ("2. Crear fichero BaseSystem.img")
+print ("3. Crear disco")
+print ("4. Instalar/Iniciar MacOS 13 Ventura")
+print ("5. Salir")
+
+opcion = int(input("Elige una opcion: "))
+
+if opcion == 1:
+	os.system("bash InstalarSW.sh")
+elif opcion == 2:
+	os.system("bash CrearBaseSystemImg.sh")
+elif opcion == 3:
+	os.system("bash CrearDisco.sh")
+elif opcion == 4:
+	os.system("bash InstalarMacOS.sh")
+
